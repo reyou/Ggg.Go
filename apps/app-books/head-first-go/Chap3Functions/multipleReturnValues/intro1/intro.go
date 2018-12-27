9154aa7a3737d1ba173937caa911a69f6b0d8e5b
aozdemir <aozdemir@webmd.net>
2018-12-27 11:08:36 -0500

    page 106

---
+++ apps/app-books/head-first-go/Chap3Functions/multipleReturnValues/intro1/intro.go
@@ -0,0 +1,13 @@
+// multiple return values
+package main
+
+import "fmt"
+
+func manyReturns() (int, bool, string) {
+	return 1, true, "hello"
+}
+
+func main() {
+	myInt, myBool, myString := manyReturns()
+	fmt.Println(myInt, myBool, myString)
+}
---
+++ apps/app-books/head-first-go/Chap3Functions/multipleReturnValues/intro2/intro.go
@@ -0,0 +1,17 @@
+// multiple return values
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func floatParts(number float64) (integerPart int, fractionalPart float64) {
+	wholeNumber := math.Floor(number)
+	return int(wholeNumber), number - wholeNumber
+}
+
+func main() {
+	cans, remainder := floatParts(1.26)
+	fmt.Println(cans, remainder)
+}
